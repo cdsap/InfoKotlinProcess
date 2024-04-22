492ebcc6e5c03224d4c72e44a3daa4540c75bb31
Inaki Villar <inaki.seri@gmail.com>
2024-04-22 19:26:49 +0200

    using develocity api

--- src/main/kotlin/io/github/cdsap/kotlinprocess/InfoKotlinProcessPlugin.kt
+++ src/main/kotlin/io/github/cdsap/kotlinprocess/InfoKotlinProcessPlugin.kt
@@ -1,22 +1,31 @@
 package io.github.cdsap.kotlinprocess
 
+import com.gradle.develocity.agent.gradle.DevelocityConfiguration
 import com.gradle.scan.plugin.BuildScanExtension
 import io.github.cdsap.jdk.tools.parser.ConsolidateProcesses
+import io.github.cdsap.jdk.tools.parser.model.Process
 import io.github.cdsap.jdk.tools.parser.model.TypeProcess
-import io.github.cdsap.kotlinprocess.output.BuildScanOutput
+import io.github.cdsap.kotlinprocess.output.DevelocityValues
+import io.github.cdsap.kotlinprocess.output.EnterpriseValues
 import io.github.cdsap.valuesourceprocess.jInfo
 import io.github.cdsap.valuesourceprocess.jStat
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.provider.Provider
 
 
 class InfoKotlinProcessPlugin : Plugin<Project> {
     private val nameProcess = "KotlinCompileDaemon"
     override fun apply(target: Project) {
         target.gradle.rootProject {
-            val buildScanExtension = extensions.findByType(com.gradle.scan.plugin.BuildScanExtension::class.java)
-            if (buildScanExtension != null) {
-                buildScanReporting(project, buildScanExtension)
+
+            val develocityConfiguration = extensions.findByType(DevelocityConfiguration::class.java)
+            val enterpriseExtension = extensions.findByType(com.gradle.scan.plugin.BuildScanExtension::class.java)
+
+            if (develocityConfiguration != null) {
+                buildScanDevelocityReporting(project, develocityConfiguration)
+            } else if (enterpriseExtension != null) {
+                buildScanEnterpriseReporting(project, enterpriseExtension)
             } else {
                 consoleReporting(target)
             }
@@ -32,16 +41,41 @@ class InfoKotlinProcessPlugin : Plugin<Project> {
         }.get()
     }
 
-    private fun buildScanReporting(
+    private fun buildScanEnterpriseReporting(
         project: Project,
         buildScanExtension: BuildScanExtension
     ) {
-        val jStat = project.jStat(nameProcess)
-        val jInfo = project.jInfo(nameProcess)
+        val (jStat, jInfo) = providerPair(project)
 
         buildScanExtension.buildFinished {
-            val processes = ConsolidateProcesses().consolidate(jStat.get(), jInfo.get(), TypeProcess.Kotlin)
-            BuildScanOutput(buildScanExtension, processes).addProcessesInfoToBuildScan()
+            val processes = processes(jStat, jInfo)
+            EnterpriseValues(buildScanExtension, processes).addProcessesInfoToBuildScan()
         }
+    }
+
+    private fun buildScanDevelocityReporting(
+        project: Project,
+        buildScanExtension: DevelocityConfiguration
+    ) {
+        val (jStat, jInfo) = providerPair(project)
+
+        buildScanExtension.buildScan.buildFinished {
+            val processes = processes(jStat, jInfo)
+            DevelocityValues(buildScanExtension, processes).addProcessesInfoToBuildScan()
+        }
+    }
+
+    private fun processes(
+        jStat: Provider<String>,
+        jInfo: Provider<String>
+    ): List<Process> {
+        val processes = ConsolidateProcesses().consolidate(jStat.get(), jInfo.get(), TypeProcess.Kotlin)
+        return processes
+    }
+
+    private fun providerPair(project: Project): Pair<Provider<String>, Provider<String>> {
+        val jStat = project.jStat(nameProcess)
+        val jInfo = project.jInfo(nameProcess)
+        return Pair(jStat, jInfo)
     }
 }
---
+++ src/main/kotlin/io/github/cdsap/kotlinprocess/output/DevelocityValues.kt
@@ -0,0 +1,23 @@
+package io.github.cdsap.kotlinprocess.output
+
+import com.gradle.develocity.agent.gradle.DevelocityConfiguration
+import io.github.cdsap.jdk.tools.parser.model.Process
+
+class DevelocityValues(
+    private val develocityConfiguration: DevelocityConfiguration,
+    private val processes: List<Process>,
+) {
+
+    fun addProcessesInfoToBuildScan() {
+        processes.map {
+            develocityConfiguration.buildScan {
+                value("Kotlin-Process-${it.pid}-max", "${it.max} GB")
+                value("Kotlin-Process-${it.pid}-usage", "${it.usage} GB")
+                value("Kotlin-Process-${it.pid}-capacity", "${it.capacity} GB")
+                value("Kotlin-Process-${it.pid}-uptime", "${it.uptime} minutes")
+                value("Kotlin-Process-${it.pid}-gcTime", "${it.gcTime} minutes")
+                value("Kotlin-Process-${it.pid}-gcType", it.typeGc)
+            }
+        }
+    }
+}
