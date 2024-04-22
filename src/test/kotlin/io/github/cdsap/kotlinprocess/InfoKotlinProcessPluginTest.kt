768ddcdaab255a084d0476afbd326d6035def0c4
Inaki Villar <inaki.seri@gmail.com>
2024-04-22 19:57:23 +0200

    adding correct Gradle test version

--- src/test/kotlin/io/github/cdsap/kotlinprocess/InfoKotlinProcessPluginTest.kt
+++ src/test/kotlin/io/github/cdsap/kotlinprocess/InfoKotlinProcessPluginTest.kt
@@ -10,7 +10,7 @@ import org.junit.rules.TemporaryFolder
 
 class InfoKotlinProcessPluginTest {
 
-    private val gradleVersions = listOf("7.6.2", "8.1.1", "8.6.2", "8.7")
+    private val gradleVersions = listOf("7.6.2", "8.1.1", "8.6", "8.7")
 
     @Rule
     @JvmField
