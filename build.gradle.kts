f0e08997c3f94fdc9fc4f27f85d5125de3c2e1bf
Inaki Villar <inaki.seri@gmail.com>
2025-06-11 15:46:03 -0700

    preparing 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "io.github.cdsap"
-version = "0.1.7"
+version = "0.2.0"
 
 java {
     toolchain {
--- src/test/kotlin/io/github/cdsap/kotlinprocess/InfoKotlinProcessPluginWtihBuildScanTest.kt
+++ src/test/kotlin/io/github/cdsap/kotlinprocess/InfoKotlinProcessPluginWtihBuildScanTest.kt
@@ -47,7 +47,7 @@ class InfoKotlinProcessPluginWtihBuildScanTest {
 
             """.trimIndent()
         )
-        listOf("8.1.1").forEach {
+        listOf("8.14.2").forEach {
             val firstBuild = GradleRunner.create()
                 .withProjectDir(testProjectDir.root)
                 .withArguments("compileKotlin", "--configuration-cache")
